8c0928ec2fb3e974d7c8361136892e7450d92019
cmk5809 <cmk5809@naver.com>
2020-01-13 09:11:32 +0900

    aa

---
+++ CallEx/src/com/naver/Test.java
@@ -0,0 +1,32 @@
+package com.naver;
+
+public class Test {
+	private int count;
+	
+	public void sleep() {
+		System.out.println("¿‚¥œ¥Ÿ.");
+	}
+	
+	public void me11() {
+		shot();
+	}
+	
+	public void shot() {
+		System.out.println("√—¿ª Ωı¥œ¥Ÿ.");
+		count = count+1;
+		System.out.println(count);
+	}
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+
+}
---
+++ CallEx/src/kr/co/ca/MainEx4.java
@@ -0,0 +1,19 @@
+package kr.co.ca;
+
+import com.naver.Test;
+
+public class MainEx4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Test daum = new Test();
+		int c = daum.getCount();
+		System.out.println("::::::::::");
+		
+		
+		daum.setCount(24);
+		System.out.println(daum.getCount());
+		}
+
+	}
+
